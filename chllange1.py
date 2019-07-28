00ac20c3d4f367fc98a116b1a64f0d27bb3b8225
chrisribia <31062468+chrisribia@users.noreply.github.com>
2019-07-28 16:42:11 +0300

    Add files via upload

---
+++ chllange1.py
@@ -0,0 +1,25 @@
+lst = [3, 5, -4, 8, 11, 1, -1, 6] 
+def sum_two(lst,sum):
+    """
+    ARGS:
+        function takes list as argument
+
+    Returns:
+        returns a list of two numbers of sum 10  
+
+    Run Time:
+        O(n)
+
+    """
+    n = {x:sum-x for x in lst if (sum - x) in lst  and (x - sum) in lst}
+    print(n)
+
+sum_two(lst,sum = 10)
+ 
+ 
+ 
+
+
+    
+     
+ 
