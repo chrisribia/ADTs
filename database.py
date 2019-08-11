21d10c52bfd1138d5d1d90d3b45230ed22fe5a52
chrisribia <31062468+chrisribia@users.noreply.github.com>
2019-08-11 18:32:06 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,65 @@
+from abc import (ABC, abstractmethod)
+import psycopg2
+
+
+class Postgres(ABC):
+   
+    @abstractmethod
+    def connect(self,user, password, host, port, database):
+        conn = psycopg2.connect(host,database,user,password)
+        return conn
+
+    @abstractmethod
+    def session(self):
+        autocommit = True
+        return self.connect.commit()
+
+    @abstractmethod
+    def create_database(self):
+        pass
+
+    @abstractmethod
+    def status(self):
+        pass
+
+    @abstractmethod
+    def cursor(self,query):
+        return self.session.cursor(query)
+
+    @abstractmethod
+    def select_table(self,query):
+        pass
+
+    @abstractmethod
+    def create_table(self,query):             
+        try:
+            for query in queries:
+                cursor.execute(query)
+            self.session            
+            print('Creating Tables.....Done')
+        except:
+            print("Failed to Create tables")   
+
+    @abstractmethod
+    def insert_rows(self,query):
+        conn = psycopg2.connect(query)
+        cur = conn.cursor()
+        self.session  
+
+    @abstractmethod
+    def show_table(self,query):
+        pass
+
+    @abstractmethod
+    def drop_table(self,query):
+        conn = connection(DATABASE_URL)
+        cursor = conn.cursor()        
+        try:        
+            self.cursor(query)
+            self.session                
+        except:
+            print("Failed to Destroy tables")
+
+    @abstractmethod
+    def close(self):
+        pass
